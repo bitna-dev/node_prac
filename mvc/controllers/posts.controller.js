72269f4ceb62c97431a2097ca9c928f0a83d1232
Bitna Lee <bitna@Bitna.local>
2023-12-20 12:14:39 -0800

    mvc

--- mvc/controllers/posts.controller.js
+++ mvc/controllers/posts.controller.js
@@ -1,7 +1,12 @@
 const posts = require("../models/posts.model");
+const path = require("path");
 
 function getPosts(req, res) {
-  res.send(posts);
+  //res.send(posts);
+  //res.sendFile(path.join(__dirname, "..", "public", "images", "bitna.jpg"));
+  res.render("posts", {
+    templateName: "post",
+  });
 }
 
 module.exports = { getPosts };
--- mvc/server.js
+++ mvc/server.js
@@ -1,15 +1,35 @@
 const express = require("express");
-const userController = require("./controllers/users.controller");
-const postController = require("./controllers/posts.controller");
+const path = require("path");
+const postsRouter = require("./routes/posts.router");
+const usersRouter = require("./routes/users.router");
+
 const PORT = 3000;
 const app = express();
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`Start ${req.method} ${req.url}`);
+  next();
+  const diffTime = Date.now() - start;
+  console.log(`end: ${req.method} ${req.baseUrl} ${req.url} ${diffTime}`);
+});
 // body parsing middleware
 app.use(express.json());
 
-app.get("/users", userController.getUsers);
-app.get("/user/:id", userController.getUser);
-app.post("/users", userController.postUser);
-app.get("/posts", postController.getPosts);
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+
+//template engine과 결합
+app.get("/", (req, res) => {
+  res.render("index", {
+    imageTitle: "This is Node.js Application",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Express ${PORT} conducted`);
